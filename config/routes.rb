35c6fb5f1148e4266b1f6d330e2b9e0e5b1994b9
Carlos Gil <carlos.gil@koonolmexico.com>
2025-02-17 10:20:34 +0000

    seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :v1 do
+    resources :users, only: [:create]
+    resources :posts, only: [:create]
+    resources :ratings, only: [:create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,54 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+require 'open-uri'
+require 'json'
+
+def create_user(login)
+  response = `curl -X POST http://localhost:3000/v1/users -d "user[login]=#{login}"`
+  JSON.parse(response)
+end
+
+def create_post(user_id, title, body, ip)
+  response = `curl -X POST http://localhost:3000/v1/posts -d "post[user_id]=#{user_id}&post[title]=#{title}&post[body]=#{body}&post[ip]=#{ip}"`
+  JSON.parse(response)
+end
+
+def create_rating(post_id, user_id, value)
+  response = `curl -X POST http://localhost:3000/v1/ratings -d "rating[post_id]=#{post_id}&rating[user_id]=#{user_id}&rating[value]=#{value}"`
+  JSON.parse(response)
+end
+
+100.times do |i|
+  login = Faker::Internet.unique.username
+  create_user(login)
+end
+
+ips = []
+50.times do |i|
+  ips << "192.168.1.#{i+1}"
+end
+
+post_created = 0
+200_000.times do |i|
+  user = User.order('RANDOM()').first
+  title = Faker::Lorem.sentence
+  body = Faker::Lorem.paragraph
+  ip = ips.sample
+
+  post_data = create_post(user.id, title, body, ip)
+  post_id = post_data['post']['id']
+  post_created += 1
+
+  if post_created <= 160000
+    users_that_voted = []
+    8.times do
+      user_voter = User.order('RANDOM()').first
+      next if users_that_voted.include?(user_voter.id)
+
+      value = rand(1..5)
+      create_rating(post_id, user_voter.id, value)
+      users_that_voted << user_voter.id
+    end
+  end
+end
+
+puts "Created 100 users, 200k posts."
