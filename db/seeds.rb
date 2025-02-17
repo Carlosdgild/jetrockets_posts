184ec983ec711a3c2179385a032f9c2aaad3acc7
Carlos Gil <carlos.gil@koonolmexico.com>
2025-02-17 23:47:40 +0000

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,17 @@ require 'open-uri'
 require 'json'
 
 def create_user(login)
-  response = `curl -X POST http://localhost:3000/v1/users -d "user[login]=#{login}"`
+  response = `curl -X POST http://localhost:3000/users -d "user[login]=#{login}"`
   JSON.parse(response)
 end
 
-def create_post(user_id, title, body, ip)
-  response = `curl -X POST http://localhost:3000/v1/posts -d "post[user_id]=#{user_id}&post[title]=#{title}&post[body]=#{body}&post[ip]=#{ip}"`
+def create_post(login, title, body, ip)
+  response = `curl -X POST http://localhost:3000/posts -d "post[login]=#{login}&post[title]=#{title}&post[body]=#{body}&post[ip]=#{ip}"`
   JSON.parse(response)
 end
 
 def create_rating(post_id, user_id, value)
-  response = `curl -X POST http://localhost:3000/v1/ratings -d "rating[post_id]=#{post_id}&rating[user_id]=#{user_id}&rating[value]=#{value}"`
+  response = `curl -X POST http://localhost:3000/ratings -d "rating[post_id]=#{post_id}&rating[user_id]=#{user_id}&rating[value]=#{value}"`
   JSON.parse(response)
 end
 
@@ -28,19 +28,19 @@ ips = []
 end
 
 post_created = 0
-200_000.times do |i|
+200000.times do |i|
   user = User.order('RANDOM()').first
   title = Faker::Lorem.sentence
   body = Faker::Lorem.paragraph
   ip = ips.sample
 
-  post_data = create_post(user.id, title, body, ip)
-  post_id = post_data['post']['id']
+  post_data = create_post(user.login, title, body, ip)
+  post_id = post_data['id']
   post_created += 1
 
   if post_created <= 160000
     users_that_voted = []
-    8.times do
+    3.times do
       user_voter = User.order('RANDOM()').first
       next if users_that_voted.include?(user_voter.id)
 
